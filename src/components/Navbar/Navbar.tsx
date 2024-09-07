0daf5185eab0f71ab78f1e36dd74e0d5b55b7d0e
Kerem Mert <kerem37mert@yahoo.com>
2024-09-07 20:35:07 +0300

    Navbar updated

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,16 @@
+import React from "react";
 import "./Navbar.css";
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
     return(
-        <div>Navbar</div>
+        <div className="navbar">
+            <a className="navbar-title" href="/">Kipl</a>
+            <div className="navbar-links">
+                <a href="">Download</a>
+                <a href="">Documentation</a>
+                <a href="">Community</a>
+            </div>
+        </div>
     );
 }
 
