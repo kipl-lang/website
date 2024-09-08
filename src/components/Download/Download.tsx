c4758e5faecf9db4d9fcd3da8e00b065e80f8bd1
Kerem Mert <kerem37mert@yahoo.com>
2024-09-08 20:49:38 +0300

    Download updated

--- src/components/Download/Download.tsx
+++ src/components/Download/Download.tsx
@@ -4,7 +4,8 @@ import "./Download.css";
 const Download:React.FC = () => {
     return(
         <div className="download">
-            akjsfhıas
+            <h2 className="download-title">Download Kipl</h2>
+            <p className="download-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, molestiae!</p>
         </div>
     );
 }
