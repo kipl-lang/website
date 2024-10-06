ea796a85bbab8ef69af616b3b09b48420bf45452
Kerem Mert <kerem37mert@yahoo.com>
2024-10-06 11:32:48 +0300

    AppBar updated

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,9 +1,16 @@
 import "./AppBar.css";
 
+import { Link } from "react-router-dom";
+
 const AppBar = () => {
     return(
-        <div>
-            AppBar
+        <div className={"app-bar"}>
+            <Link to={"/"} className={"app-bar-title"}>Kipl</Link>
+            <div className={"app-bar-links-container"}>
+                <Link className={"app-bar-link"}>Get Started</Link>
+                <Link className={"app-bar-link"}>Documentation</Link>
+                <Link className={"app-bar-link"}>Community</Link>
+            </div>
         </div>
     );
 }
