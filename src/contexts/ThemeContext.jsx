1c58165b743419a93aaf756904187fd83d273e68
Kerem Mert <kerem37mert@yahoo.com>
2024-10-06 10:40:18 +0300

    react-router-dom added

---
+++ src/contexts/ThemeContext.jsx
@@ -0,0 +1,19 @@
+import {createContext, useState} from "react";
+
+const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+
+    const [theme, setTheme] = useState("light");
+
+    return(
+        <ThemeContext.Provider value={{ theme, setTheme }}>
+            { children }
+        </ThemeContext.Provider>
+    );
+}
+
+export {
+    ThemeContext,
+    ThemeProvider
+}
