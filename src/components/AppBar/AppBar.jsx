50f0a9e57452ace6a8414e1b2e5f22cd37756e3a
Kerem Mert <kerem37mert@yahoo.com>
2024-10-07 10:25:50 +0300

    AppBar updated

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -4,18 +4,20 @@ import { Link } from "react-router-dom";
 // Components
 import MenuButton from "./MenuButton";
 import {useContext} from "react";
-import {MenuContext} from "../../contexts/MenuContext.jsx";
+import { MenuContext } from "../../contexts/MenuContext.jsx";
 
 const AppBar = () => {
 
     const { isMenuOpen } = useContext(MenuContext);
     return(
         <div className={"app-bar"}>
-            <Link to={"/"} className={"app-bar-title"}>Kipl</Link>
+            <div className={"app-bar-title-container"}>
+                <Link to={"/"} className={"app-bar-title"}>Kipl</Link>
+            </div>
             <MenuButton />
-            <div className={"app-bar-links-container"}  style={{display: isMenuOpen && "flex"}}>
-                <Link to={"/"} className={"app-bar-link"}>Get Started</Link>
-                <Link to={"/"} className={"app-bar-link"}>Documentation</Link>
+            <div className={`app-bar-links ${isMenuOpen && "app-bar-links-open"}`}>
+                <Link to={"/"} className={"app-bar-link"}>Docs</Link>
+                <Link to={"/"} className={"app-bar-link"}>Examples</Link>
                 <Link to={"/"} className={"app-bar-link"}>Community</Link>
             </div>
         </div>
