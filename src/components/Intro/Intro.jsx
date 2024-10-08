dbb871949fec678b43b613d79c0a5e298c501517
Kerem Mert <kerem37mert@yahoo.com>
2024-10-08 17:44:50 +0300

    IntroDownload created

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,11 +1,13 @@
 import "./intro.css";
 import introIMG from "../../assets/images/intro.png"
+import IntroDownload from "./IntroDownload";
 
 const Intro = () => {
     return(
         <div className={"intro"}>
             <div className={"intro-desc-container"}>
                 <h1 className={"intro-desc-title"}>Your Favourite Programming Language</h1>
+                <IntroDownload />
             </div>
             <div className={"intro-img-container"}>
                 <img src={ introIMG } alt="introduction to kipl"/>
---
+++ src/components/Intro/IntroDownload/IntroDownload.jsx
@@ -0,0 +1,11 @@
+import "./introDownload.css";
+
+import { Link } from "react-router-dom";
+
+const IntroDownload = () => {
+    return(
+        <Link to={"/"}>Download</Link>
+    );
+}
+
+export default IntroDownload;
