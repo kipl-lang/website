4c79b733f40f206a85fd007d0c93f4525e48b6aa
Kerem <kerem37mert@yahoo.com>
2024-10-16 13:27:00 +0300

    FootBar updated

--- src/components/FootBar/Footbar.jsx
+++ src/components/FootBar/Footbar.jsx
@@ -1,9 +1,31 @@
 import "./footBar.css";
+import { Link } from "react-router-dom";
+import { GitHub, Reddit, X } from "@mui/icons-material";
+import footLogo from "../../assets/images/footbar-logo.png";
 
 const FootBar = () => {
     return(
         <div className={"foot-bar"}>
-            Kipl
+            <div className={"foot-bar-links"}>
+                <Link to={"/"} >
+                    <img
+                        src={ footLogo }
+                        alt="Kipl is programming language"
+                        className={"foot-bar-links-logo"}
+                    />
+                </Link>
+            </div>
+            <div className={"foot-bar-social"}>
+                <Link to={""} className={"foot-bar-social-item"}>
+                    <GitHub />
+                </Link>
+                <Link to={""} className={"foot-bar-social-item"}>
+                    <Reddit />
+                </Link>
+                <Link to={""} className={"foot-bar-social-item"}>
+                    <X />
+                </Link>
+            </div>
         </div>
     );
 }
